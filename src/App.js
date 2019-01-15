5e5d20d24a7339c1e5097ea6695346256e8fdfba
Prem Anand <prem2282@gmail.com>
2019-01-15 21:22:00 +0530

    Signed-off-by: Prem Anand <prem2282@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import BaseRouter from './routes';
+import AdSense from 'react-adsense';
 
 import './App.css';
 
@@ -12,6 +13,13 @@ class App extends Component {
         <Router>
           <div>
             <BaseRouter/>
+              <AdSense.Google
+                client='ca-pub-9863004487978817'
+                slot='7763046972'
+                style={{ display: 'block' }}
+                layout='in-article'
+                format='fluid'
+              />
           </div>
         </Router>
       </div>
